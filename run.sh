cd6f2b3af1bac4ed70c6c1472589e91e707c86d7
Ubuntu <ubuntu@ip-172-31-25-180.ap-southeast-1.compute.internal>
2025-04-04 11:59:48 +0000

    hehe

--- run.sh
+++ run.sh
@@ -2,7 +2,13 @@
 echo "download world"
 ./download_world.sh
 echo "running server"
-./start.bat || true
+if [ "$1" == "window" ]; then
+    copy server.win.properties server.properties
+    ./start_win.bat
+else
+    cp server.linux.properties server.properties
+    ./start_linux.bat
+fi
 
 echo "sync server"
 ./update.bat
